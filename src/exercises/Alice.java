56d7308583dff9cba07edc5a72c8aa8699ed3bec
Nick <nicholastaylor181@gmail.com>
2020-04-18 23:26:06 -0500

    finished part 5

--- src/exercises/Alice.java
+++ src/exercises/Alice.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class Alice {
     public static void main(String[] args){
+        String cutQuote;
         String quote = "\n" +
                 "\n" +
                 "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'\n";
@@ -11,7 +12,10 @@ public class Alice {
          System.out.println("Search term thats in quote:");
          String term = input.nextLine();
         if (quote.toLowerCase().contains(term.toLowerCase())) {
-            System.out.println("true");
+            System.out.println("index: " + quote.toLowerCase().indexOf(term.toLowerCase()) + " length: " + term.length());
+            cutQuote = quote.toLowerCase().replace(term.toLowerCase(), "");
+            System.out.println(cutQuote);
+
         } else {
             System.out.println("false");
         }
