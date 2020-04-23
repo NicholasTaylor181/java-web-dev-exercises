9fd9e837b46e7ce722c0266f81c16209d0d2c688
Nick <nicholastaylor181@gmail.com>
2020-04-23 18:55:39 -0500

    Finished Studio 1

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,16 @@
+package org.launchcode.java.studios.areaofacircle;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.println("Enter the radius of a circle: ");
+        double radius = input.nextDouble();
+        double area = Circle.getArea(radius);
+        System.out.println(area);
+    }
+}
